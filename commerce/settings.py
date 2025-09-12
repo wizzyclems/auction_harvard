d0d9dc6c6e0d4cf22d16a22fbcb653be14591440
Ifiok Udoh <ifiokudoh01@gmail.com>
2025-09-13 00:03:03 +0100

    change the location of the sqlite database file

--- commerce/settings.py
+++ commerce/settings.py
@@ -87,7 +87,8 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': '/home/app_user/db/db.sqlite3'
     }
 }
 
